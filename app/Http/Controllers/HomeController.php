f2119defd5d7d124e32543581c315b9ffbcd665f
Sijanbhattarai <bhattaraisijan@outlook.com>
2020-01-02 10:11:16 +0545

    Minor

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,6 @@ class HomeController extends Controller
 {
     public function home()
     {
-    	return view('frontend.home', compact('scats','smode'));
+    	return view('frontend.home');
     }
 }
